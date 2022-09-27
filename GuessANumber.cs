91c1834831b0f466812a636753931ba1625321dd
Tonislav <pichaaga@abv.bg>
2022-09-27 10:02:01 +0300

    Add project Guess A Number

---
+++ GuessANumber.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace GuessTheNumberByToni
+{
+    internal class GuessANumber
+    {
+        static void Main(string[] args)
+        {
+            Random randomNumber = new Random();
+            int computerNumber = randomNumber.Next(1, 101);
+            while (true)
+            {
+                Console.Write("Guess the number (1-100): ");
+                string playersinput = Console.ReadLine();
+                bool isValid = int.TryParse(playersinput, out int playerNumber);
+                if (isValid)
+                {
+                    if (playerNumber==computerNumber)
+                    {
+                        Console.WriteLine("You guessed it!");
+                        break;
+                    }
+                    else if (playerNumber>computerNumber)
+                    {
+                        Console.WriteLine("Too High");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Too Low");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input.");
+                }
+            }
+        }
+    }
+}
